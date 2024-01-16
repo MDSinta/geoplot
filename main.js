f08e1655b2a3a3d570487b50c6324434a40675cb
MDsinta <mdtasinr2789@nycstudents.net>
2024-01-16 15:23:26 -0500

    123123123

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 var map = L.map('map').setView([0, 0], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,8 +14,9 @@ fileinput.onchange = getExif;
 // https://github.com/ianare/exif-samples/blob/master/jpg/Kodak_CX7530.jpg <-- source for the image
 // https://github.com/exif-js/exif-js/blob/master/exif.js <-- exif.js original readme code, modified by me
 function getExif() {
-    var img1 = getFile();
-    EXIF.getData(img1, function() {
+    const file = document.getElementById("files").files[0]
+    createImage(file)
+    EXIF.getData(file, function() {
         var longitude = EXIF.getTag(this, "GPSLongitude");
         var latitude = EXIF.getTag(this, "GPSLatitude");
         var geolocation = document.getElementById("geo");
@@ -33,9 +36,9 @@ const placeMarker = (a, b) => {
     var point = L.marker([a, b]).addTo(map);
 }
 
-async function getFile() {
-    console.log("worked")
-    const files = document.getElementById("files").files[0]
-    const blob = await files.blob()
-    return blob;
+const createImage = (a) => {
+    const images = document.createElement("img")
+    const placement = document.getElementById('one')
+    images.src=URL.createObjectURL(a)   ;
+    placement.appendChild(images)
 }
